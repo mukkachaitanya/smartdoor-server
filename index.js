45cc0c255397517338a15ddef5ef855abdbf4f37
Chaitanya Mukka <chaitanya.mvs2007@gmail.com>
2018-10-23 19:54:14 +0530

    More changes

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require('express');
 const app = express();
+const multer = require('multer');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
-app.use(bodyParser.json({limit: '50mb', extended: true},)); // for parsing application/json
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true},)); // for parsing application/x-www-form-urlencoded
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: './uploads/',
+        filename: function (req, file, cb){
+            // user shortid.generate() alone if no extension is needed
+            cb(null, path.parse(file.originalname).ext);
+        }
+    })
+});
+
+// app.use(bodyParser.json()); // for parsing application/json
+// app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 
 const port = 3000;
 
 app.post('/upload', (req, res) => {
-    console.log(req.body);
-    res.send("Verified");
+    
+    if(!req.body){
+        res.end();
+    }
+
+    let img_encoded = req.body;
+    let img = img_encoded.replace(/^data:image\/png;base64,/, "");
+
+    fs.writeFile("./upload/img.jpg", img, 'base64', function(err) {
+        const { spawn } = require('child_process');
+        const pyProg = spawn('python2', ['./../model/fig_chk.py', 'hell']);
+
+        pyProg.stdout.on('data', function(data) {
+            console.log(data.toString());
+            res.write(data);
+            res.end();
+        });
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
